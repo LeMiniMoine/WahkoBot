828c7f838691ef47fe1b943326511faca31d662d
LeMiniMoine <mdauph1@gmail.com>
2025-06-03 10:00:33 +0200

    Create helloworld.py

---
+++ root/cogs/helloworld/helloworld.py
@@ -0,0 +1,13 @@
+import discord
+from redbot.core import commands
+
+class TestCmd(commands.Cog):
+    """A simple test command cog."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def hello(self, ctx):
+        """Replies with a friendly hello."""
+        await ctx.send("👋 Hello! The cog is working correctly.")
